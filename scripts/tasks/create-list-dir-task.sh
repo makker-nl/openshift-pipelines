9459ef1629d9c6f2637697a755e60384aeaf52a3
Martien van den Akker <martien.van.den.akker@virtualsciences.nl>
2021-02-15 15:31:18 +0100

    Add create list dir task.

---
+++ scripts/tasks/create-list-dir-task.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SCRIPTPATH=$(dirname $0)
+. $SCRIPTPATH/../bin/oc_env.sh
+# Create a task  to list a directory from a persistent volume.
+TASK=list-directory
+echo Delete old task $TASK
+kubectl delete task $TASK
+echo Create new task $TASK
+kubectl apply -n $NS -f list-directory-task.yml
+echo Get new task $TASK
+kubectl get task $TASK -o yaml
